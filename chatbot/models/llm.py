320c04ebaa0b12d11aeb767e3f212bee1153963a
Benjamin Bolte <ben@bolte.cc>
2023-06-22 20:39:55 +0000

    cleanup

--- chatbot/models/llm.py
+++ chatbot/models/llm.py
@@ -19,12 +19,7 @@ class ChatbotModel(ml.BaseModel[ChatbotModelConfig]):
     def __init__(self, config: ChatbotModelConfig) -> None:
         super().__init__(config)
 
-        self.rwkv = pretrained_rwkv(
-            "430m",
-            lora_rank=config.lora_rank,
-            # wkv_impl="triton-eps",
-            wkv_impl="eps",
-        )
+        self.rwkv = pretrained_rwkv("430m", lora_rank=config.lora_rank, wkv_impl="log")
         self.predictor = self.rwkv.predictor()
 
     def forward(self, tokens: Tensor) -> Tensor:
