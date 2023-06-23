4478b8144d12417991c964aac2acd0f0d29b5533
Benjamin Bolte <ben@bolte.cc>
2023-06-23 20:34:04 +0000

    log kernel experiments

--- chatbot/models/rwkv.py
+++ chatbot/models/rwkv.py
@@ -19,7 +19,7 @@ class RwkvChatbotModel(ml.BaseModel[RwkvChatbotModelConfig]):
     def __init__(self, config: RwkvChatbotModelConfig) -> None:
         super().__init__(config)
 
-        self.rwkv = pretrained_rwkv("430m", lora_rank=config.lora_rank, wkv_impl="eps")
+        self.rwkv = pretrained_rwkv("430m", lora_rank=config.lora_rank, wkv_impl="log")
         self.predictor = self.rwkv.predictor()
 
     def forward(self, tokens: Tensor) -> Tensor:
