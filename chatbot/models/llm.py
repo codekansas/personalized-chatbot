dccf918c14038746d994f5e40115f5ffe67d1e6e
Ben Bolte <ben@bolte.cc>
2023-06-22 22:41:58 -0400

    use ml-rwkv package (#6)
    * use ml-rwkv package
    
    * cleanup
    
    * use eps instead of log

--- chatbot/models/llm.py
+++ chatbot/models/llm.py
@@ -5,7 +5,7 @@ from typing import Iterator, Sequence
 
 import ml.api as ml
 import torch
-from pretrained.rwkv import pretrained_rwkv
+from rwkv.model import pretrained_rwkv
 from torch import Tensor
 
 
@@ -19,7 +19,7 @@ class ChatbotModel(ml.BaseModel[ChatbotModelConfig]):
     def __init__(self, config: ChatbotModelConfig) -> None:
         super().__init__(config)
 
-        self.rwkv = pretrained_rwkv("430m", lora_rank=config.lora_rank)
+        self.rwkv = pretrained_rwkv("430m", lora_rank=config.lora_rank, wkv_impl="eps")
         self.predictor = self.rwkv.predictor()
 
     def forward(self, tokens: Tensor) -> Tensor:
